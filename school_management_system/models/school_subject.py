a2190b6f45aa49e1b61c2b3bb69205541b63e914
Nancy Elghoal <engnancy.reda1@gmail.com>
2023-07-02 23:24:56 +0300

    adding advanced views

--- school_management_system/models/school_subject.py
+++ school_management_system/models/school_subject.py
@@ -6,6 +6,7 @@ class SchoolSubject(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
     name= fields.Char('Subject', required=True)
-    year_id= fields.Many2one(comodel_name="school.year", string="Year", required=True)
+    year_id= fields.Many2one(comodel_name="school.year", string="Year")
     student_id= fields.Many2one(comodel_name="school.student", string="Student", required=True)
+
 
